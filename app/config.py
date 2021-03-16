58fd2d5487ed408b70e56a81c4a9944153879704
Azhar Basit <azhar.basit@gmail.com>
2021-03-16 16:01:31 +0900

    Changed redis address from localhost to redis

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 UPLOAD_FOLDER = 'flask-celery-pregen/input-images'
 RESULT_FOLDER = 'flask-celery-pregen/preview-images'
-BROKER = 'redis://localhost:6379/0'
-BACKEND = 'redis://localhost:6379/0'
+BROKER = 'redis://redis:6379/0'
+BACKEND = 'redis://redis:6379/0'
