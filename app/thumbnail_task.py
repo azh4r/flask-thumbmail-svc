20974581b1922be1f456b70388bc52ca3f9af09e
Azhar Basit <azhar.basit@gmail.com>
2021-04-21 04:22:04 +0900

    Adding comments.  Catching exceptions and returning error messages based on exceptions. In case of successful processing returning true:

--- app/thumbnail_task.py
+++ app/thumbnail_task.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from typing import Type
+from PIL import Image, UnidentifiedImageError
 from celery import Celery
 import os
 from . import config
@@ -8,15 +9,20 @@ celery = Celery(broker=config.BROKER,backend=config.BACKEND)
 
 @celery.task(name='image.processing')
 def generate_thumbnail(filename):
-    path = os.path.abspath(os.path.join(
-           os.getcwd(), os.pardir, config.UPLOAD_FOLDER, filename))
-    image = Image.open(path)
-    file_path = os.path.abspath(os.path.join(
-            os.getcwd(), os.pardir, config.RESULT_FOLDER, filename))
-    image.thumbnail((100,100))
-    image.save(file_path)
+    try: 
+        # Get the file path to the folder with uploaded file
+        uploaded_file = os.path.abspath(os.path.join(os.getcwd(), os.pardir, config.UPLOAD_FOLDER, filename))
+        image = Image.open(uploaded_file)
+        # Get the file path where the output preview file will be saved to
+        preview_file = os.path.abspath(os.path.join(os.getcwd(), os.pardir, config.RESULT_FOLDER, filename))
+        image.thumbnail((100,100))
+        image.save(preview_file)
+    except (FileNotFoundError) as ex:
+        return "File not found"
+    except (UnidentifiedImageError) as ex:
+        return "Image type unidentified in file: ".join(filename)
     # time.sleep(30)
-    return filename
+    return True
 
 if __name__ == "__main__":
     generate_thumbnail('input-images/painting_image1.jpg')
