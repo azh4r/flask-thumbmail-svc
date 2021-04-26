11c149ebc050c822c26404839fd5930b322f93dd
Azhar Basit <azhar.basit@gmail.com>
2021-04-27 04:10:51 +0900

    Added flag to generate celery logs in script and added a directory for logs.

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cd app || exit
-su -m appuser -c "celery -A app.resources.thumbnail_task.celery worker --loglevel=info"
+su -m appuser -c "celery -A app.resources.thumbnail_task.celery worker --loglevel=info --logfile=logs/celery.log"
